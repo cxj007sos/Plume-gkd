8ab93a3acf6512f7a348e55b9d960fd5da3e66ad
大羽 <96457600+cxj007sos@users.noreply.github.com>
2026-05-09 16:18:03 +0800

    Update com.baidu.BaiduMap.ts

--- src/apps/com.baidu.BaiduMap.ts
+++ src/apps/com.baidu.BaiduMap.ts
@@ -12,11 +12,8 @@ export default defineGkdApp({
       rules: [
         {
           key: 0,
-          // 核心匹配：包含“跳过”、尺寸符合右上角按钮、可见
           matches: '[text*="跳过"][width<300 && height<150][visibleToUser=true]',
-          // 上传你的百度地图截图，获取链接填在这里
           snapshotUrls: ['https://i.gkd.li/i/27548797'],
-          delay: 300, // 给动态文字渲染留一点时间
         },
       ],
     },
