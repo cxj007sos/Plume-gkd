5f0e5a011dfd1e8b9a595cc8e97d800948f516c8
大羽 <96457600+cxj007sos@users.noreply.github.com>
2026-05-09 14:47:21 +0800

    Add support for multiple ad activity IDs and snapshots

--- src/apps/com.sina.weibo.ts
+++ src/apps/com.sina.weibo.ts
@@ -8,12 +8,19 @@ export default defineGkdApp({
       key: 1,
       name: '开屏广告 - 自动跳过',
       desc: '自动点击开屏广告的跳过按钮',
-      activityIds: ['com.sina.weibo.MainTabActivity'],
+      // 同时匹配两个广告页面
+      activityIds: [
+        'com.sina.weibo.MainTabActivity',
+        'com.sina.weibo.SplashActivity'
+      ],
       rules: [
         {
           key: 0,
           matches: '[text*="跳过"][visibleToUser=true]',
-          snapshotUrls: ['https://i.gkd.li/i/27545269'],
+          snapshotUrls: [
+            'https://i.gkd.li/i/27545269',
+            'https://i.gkd.li/i/27547054'
+          ],
         },
       ],
     },
