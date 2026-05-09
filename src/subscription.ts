f4870721996683532f1860fdd81483a3503d3719
大羽 <96457600+cxj007sos@users.noreply.github.com>
2026-05-09 12:00:42 +0800

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,10 +4,10 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
+  id: 2233,
+  name: '大羽的GKD订阅-20260509',
   version: 0,
-  author: 'author',
+  author: '大羽',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/gkd-kit/subscription-template',
   categories,
