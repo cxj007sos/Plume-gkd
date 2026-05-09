7ba5c0dac7c3d522e826495232a6d9382952e45a
大羽 <96457600+cxj007sos@users.noreply.github.com>
2026-05-10 01:10:02 +0800

    Add parameters for fast query and ad skip rules

--- src/apps/com.srcb.pmbank.ts
+++ src/apps/com.srcb.pmbank.ts
@@ -5,6 +5,13 @@ export default defineGkdApp({
   name: '上海农商银行',
   groups: [
     {
+      // 解决延迟的 5 个参数
+      fastQuery: true, // 开启快速查找
+      matchTime: 10000, // 10秒内持续找
+      actionMaximum: 1, // 只点1次
+      resetMatch: 'app', // 打开APP重新计时
+      priorityTime: 10000, // 优先执行
+      
       key: 1,
       name: '开屏广告跳过',
       desc: '自动点击开屏广告的倒计时跳过按钮',
@@ -15,7 +22,7 @@ export default defineGkdApp({
         {
           key: 0,
           matches:
-            '[text*="跳过"][width<300 && height<150][visibleToUser=true]',
+            '[text*="跳过"][visibleToUser=true]',
           snapshotUrls: ['https://i.gkd.li/i/27556001'],
         },
         // 规则2：新增 VID 匹配（更快更稳）
