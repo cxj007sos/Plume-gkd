877501e3d6507b0039fe9e13a705fba1e36d3d66
大羽 <96457600+cxj007sos@users.noreply.github.com>
2026-05-27 11:17:15 +0800

    支付宝弹窗广告

--- src/apps/com.eg.android.AlipayGphone.ts
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -12,15 +12,13 @@ export default defineGkdApp({
         {
           key: 0,
           matches: 'View[width=1152][height=1728][visibleToUser=true]',
+          // 动作依然是 click
           action: 'click',
-          // 关键修正：在新版中，相对位置必须放在 actionPosition 里包裹
-          actionPosition: {
-            position: {
-              left: 532,
-              top: -969
-            }
+          // 直接作为规则根属性传入，新版规范使用 left 和 top
+          position: {
+            left: 532,
+            top: -969
           },
-          // 修正：快照链接在新版类型中通常定义为字符串数组 string[]
           snapshotUrls: ['https://i.gkd.li/i/28292384']
         }
       ]
