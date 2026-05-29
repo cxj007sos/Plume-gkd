a972d7ad824f1033a0b497e8a03b07f469762736
大羽 <96457600+cxj007sos@users.noreply.github.com>
2026-05-29 11:17:22 +0800

    支付宝

--- src/apps/com.eg.android.AlipayGphone.ts
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -2,16 +2,28 @@ import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
   id: 'com.eg.android.AlipayGphone',
-  name: '支付宝-全屏弹窗与开屏广告过滤',
-  desc: '合并了订单完成弹窗与小程序开屏广告，使用坐标自适应点击',
+  name: '支付宝',
   groups: [
     {
       key: 11,
-      name: '全屏广告-小程序及网页弹窗-坐标自适应偏移点击右上角[X]',
+      name: '支付宝-全屏广告-小程序及网页弹窗',
+      desc: '合并了订单完成弹窗与小程序开屏广告，使用坐标自适应点击',
       rules: [
         {
-          key: 0,
-          name: '订单完成-网页弹窗广告',
+          key: 0, //000 - 099	开屏类 (Splash)	启动软件时第一时间加载的广告
+          name: '开屏广告-小程序开屏-坐标自适应偏移点击左上角[关闭]',
+          fastQuery: true,
+          activityIds: 'com.alipay.mobile.nebulax.xriver.activity.XRiverActivity$',
+          position: {
+            left: 'width * 0.2490',
+            top: 'width * 0.0435',
+          },
+          matches: 'View[width=getPrev(5).width] - @ViewGroup[childCount=1][height<150][getChild(0).name$=".View"][visibleToUser=true] < [childCount>3] < ViewGroup < * < * <2 [id="android:id/content"][childCount=2]',
+          snapshotUrls: 'https://i.gkd.li/i/28337298',
+        },
+        {
+          key: 100, //100 - 199	弹窗类 (Popup)	使用过程中弹出的各类广告或引导
+          name: '订单完成-网页弹窗广告-坐标自适应偏移点击右上角[X]',
           fastQuery: true, //开启快速查询,匹配规则（matches）里面必须包含一些“能被系统直接索引的强特征”，否则成和普通的全量遍历别无二致。例如：[id="com.byted.pangle:id/tt_item_tv"]ot[text="不感兴趣"]）
           activityIds: 'com.alipay.mobile.nebulax.xriver.activity.XRiverActivity$App',
           position: { 
@@ -24,18 +36,8 @@ export default defineGkdApp({
           matches: '@ViewGroup[childCount=2] - View[top=getPrev(0).bottom] -2 View[right=width] < [childCount=4] < * < * < * < * <2 [id="android:id/content"][childCount=2]',
           snapshotUrls: 'https://i.gkd.li/i/28292384',
         },
-        {
-          key: 1,
-          name: '开屏广告-小程序开屏-坐标自适应偏移点击左上角[关闭]',
-          fastQuery: true,
-          activityIds: 'com.alipay.mobile.nebulax.xriver.activity.XRiverActivity$',
-          position: {
-            left: 'width * 0.2490',
-            top: 'width * 0.0435',
-          },
-          matches: 'View[width=getPrev(5).width] - @ViewGroup[childCount=1][height<150][getChild(0).name$=".View"][visibleToUser=true] < [childCount>3] < ViewGroup < * < * <2 [id="android:id/content"][childCount=2]',
-          snapshotUrls: 'https://i.gkd.li/i/28337298',
-        },
+
+        
       ],
     },
   ],
